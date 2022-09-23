c856b789c5e5042c319c898ea2f9468703c9d77a
unknown <Abm-Imran>
2022-09-23 11:55:49 +0600

    display country details from api

--- JS/countries.js
+++ JS/countries.js
@@ -6,8 +6,42 @@ const loadCountries = ()=>{
 loadCountries();
 
 const displayCountries = data =>{
+    console.log(data);
     const countryContainer = document.getElementById('countries')
     data.forEach( country =>{
-        console.log(country.name.official);
+        // const div = document.createElement('div');
+        const p = document.createElement('div');
+        p.classList.add('country');
+        p.innerHTML = `
+        <h3> <span style="color:black;">Country-Name: </span> ${country.name.common} </h3>
+        <p> <span ">Capital-Name: </span> ${country.capital} </p>
+        <button onclick="loadCountryDetails('${country.name.common}')"> Click Me Details </button>
+        `
+        countryContainer.appendChild(p);
     })
+}
+
+const loadCountryDetails= name =>{
+    // console.log(name);
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetails(data[0]));
+}
+
+const displayCountryDetails = country =>{
+    console.log(country);
+    const detailsBox = document.getElementById('singleCountryDetails');
+    // detailsBox = '';
+    const div = document.createElement('div');
+    div.classList.add
+    div.innerHTML=`
+    <h3> Name: ${country.name.common}</h3>
+    <p> Capital : ${country.capital}</p>
+    <img src="${country.flags.png}">
+    
+    `
+    detailsBox.appendChild(div);
+    
 }
